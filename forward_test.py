b31c56578a33d86d75d3fc56a32e3dea406ada3a
faithful777 <chukwuivnez@gmail.com>
2023-11-13 23:41:18 +0000

    commit message

---
+++ forward_test.py
@@ -0,0 +1,28 @@
+"""Before iterating over the dataset, it’s good to see what the model 
+    expects during training and inference time on sample data"""
+
+import utils
+
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
+dataset = PennFudanDataset('data/PennFudanPed', get_transform(train=True))
+data_loader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=2,
+    shuffle=True,
+    num_workers=4,
+    collate_fn=utils.collate_fn
+)
+
+# For Training
+images, targets = next(iter(data_loader))
+images = list(image for image in images)
+targets = [{k: v for k, v in t.items()} for t in targets]
+output = model(images, targets)  # Returns losses and detections
+print(output)
+
+# For inference
+model.eval()
+x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+predictions = model(x)  # Returns predictions
+print(predictions[0])
---
+++ transform.py
@@ -0,0 +1,10 @@
+from torchvision.transforms import v2 as T
+
+# helper function for data augmentation / transformation
+def get_transform(train):
+    transforms = []
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    transforms.append(T.ToDtype(torch.float, scale=True))
+    transforms.append(T.ToPureTensor())
+    return T.Compose(transforms)
